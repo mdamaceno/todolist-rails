ce405a0321bb8ceb586d16fd05cff6d95b647719
Marco Damaceno <maadamaceno@gmail.com>
2022-01-26 22:49:04 -0300

    chore: removes annoying turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
@@ -17,7 +16,6 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 //= require jquery3
 //= require jquery_ujs
-//= require turbolinks
 //= require popper
 //= require twitter/bootstrap
 //= require bootstrap-sprockets
